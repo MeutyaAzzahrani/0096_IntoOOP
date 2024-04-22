d9fb2278ab5e6be6791c531d8fbe53b82f05aefd
LENOVO <MeutyaAzzahrani>
2024-04-22 16:24:12 +0700

    menuliskan void input

--- 0096_IntoOOP/0096_IntoOOP.cpp
+++ 0096_IntoOOP/0096_IntoOOP.cpp
@@ -22,5 +22,14 @@ private:
 	string namaMk;
 	int sks;
 
+public:
+	void input() {
+		cout << "Kode MK : ";
+		cin >> kodeMk;
+		cout << "Nama MK : ";
+		cin >> namaMk;
+		cout << "SKS: ";
+		cin >> sks;
+
 }
 
