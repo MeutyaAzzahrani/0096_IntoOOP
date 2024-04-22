43391e9f28fbb638f235fd60c374fe902f4115fc
LENOVO <MeutyaAzzahrani>
2024-04-22 16:29:17 +0700

    menuliskan integer main

--- 0096_IntoOOP/0096_IntoOOP.cpp
+++ 0096_IntoOOP/0096_IntoOOP.cpp
@@ -39,3 +39,20 @@ public:
 	}
 };
 
+int main() {
+	Mahasiswa mhs;
+	Matakuliah mk;
+
+	mhs.nama = "Budi";
+	mhs.umur = 20;
+	mhs.jurusan = "Teknik Informatika";
+	mhs.output();
+
+	mk.input();
+	mk.output();
+
+	return 0;
+}
+
+
+
